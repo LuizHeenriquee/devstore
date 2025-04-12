4a6b25fad186d1b71f4b6a2585b1afd0c3b47d24
Luiz Henrique <luiz.heenrike7@gmail.com>
2025-04-12 16:41:11 -0300

    Configurei o layout padrão da aplicação, o layout de store, finalizei o cabeçalho, adicionei a biblioteca de icons Lucide React no projeto e configurei o next para suportar imagens via URL do GitHub.

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,11 +1,23 @@
 import { ReactNode } from "react";
 
 /* Components */
-import { Header } from "@/components/Header";
+import { Header } from "@/components/header";
+
+/*
+ Tailwind
+
+ mx-auto: Todo conteúdo da página é centralizado horizontalmente
+ grid: Aplica display grid
+ min-h-screen: Faz com que a div ocupe pelo menos toda a altura da tela, se ocupar mais gera barra de rolagem
+ w-full: width 100%
+ max-w-[1600px]: Em telas menores, ele não gera barra de rolagem horizontal. Em telas maiores, ele usa no máximo 1600px de largura
+ grid-rows-[min-content_max-content]: primeira linha do grid terá o menor tamanho possível e a segunda linha terá o maior
+ gap-5: 20px de espaçamento entre o cabeçalho e o conteúdo
+*/
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
-    <div>
+    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-[min-content_max-content] gap-5 p-8">
       <Header />
       {children}
     </div>
