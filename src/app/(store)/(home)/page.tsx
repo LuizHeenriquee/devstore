56823e9b2a301549844e13bbe061abfbe274bc7e
Luiz Henrique <luiz.heenrike7@gmail.com>
2025-04-17 14:40:02 -0300

    Inserindo cache

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -4,7 +4,12 @@ import { api } from "@/data/api";
 import { Product } from "@/data/types/product";
 
 async function getFeaturedProducts(): Promise<Product[]> {
-  const response = await api("/products/featured");
+  const response = await api("/products/featured", {
+    cache: "force-cache",
+    next: {
+      revalidate: 60 * 60, // 1 hora
+    },
+  });
   const products = await response.json();
 
   return products;
